f5b05bb92ad88043fdf01bdc83f16d68579adb77
chriswebdv <chriswebdv@gmail.com>
2024-09-12 17:34:47 -0600

    use prompt to get user input

--- dice.js
+++ dice.js
@@ -1,1 +1,6 @@
-console.log("Hello dice!");
+console.log("Welcome! You may begin to roll the dice");
+console.log("-----------------------------------------");
+
+let answer = prompt("Roll the dice (y/n)");
+
+console.log(answer);

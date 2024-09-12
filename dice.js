33bf073fded3a2ec255ab70ee844925a53646d88
chriswebdv <chriswebdv@gmail.com>
2024-09-12 17:53:42 -0600

    add to string method

--- dice.js
+++ dice.js
@@ -4,10 +4,17 @@ console.log("-----------------------------------------");
 let answer = prompt("Roll the dice (y/n)");
 
 //console.log(answer);
+let firstDice = Math.floor(Math.random() * 6) + 1;
+let secondDice = Math.floor(Math.random() * 6) + 1;
 
 while (true) {
   if (answer.toLowerCase() === "y") {
-    console.log("print dice result");
+    console.log(
+      "Your Result:",
+      firstDice.toString(),
+      "and",
+      secondDice.toString()
+    );
     break;
   } else {
     answer = prompt("Roll the dice (y/n)");
